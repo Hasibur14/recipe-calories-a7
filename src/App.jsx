d77be790ff6e2c98d4563f1eb49af2882ec6566c
hasibur14 <hasib141415@gmail.com>
2024-03-15 22:25:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='font-bold bg-red-600 text-7xl'>Assignment-7</h1>
+      <h1></h1>
+     
+    </>
+  )
+}
+
+export default App
